faf1692d7af3d418f016259a077f14bb5bc2dac9
888Greys <toxicgreys001@gmail.com>
2024-05-14 20:05:51 +0300

    No Sql

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Write a Python function that lists all documents in a collection:
+
+Prototype: def list_all(mongo_collection):
+"""
+
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """Return list of all docs in collection"""
+    if not mongo_collection:
+        return []
+    docs = mongo_collection.find()
+    return [doc for doc in docs]
